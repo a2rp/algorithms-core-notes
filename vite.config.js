cdbc027a7d67ec1d1a8c3cd66305ee3c403fb50e
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-02-24 12:54:10 +0530

    changed algoritms to algorithms

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/algoritms-core-notes/",
+    base: "/algorithms-core-notes/",
     plugins: [react()],
 
     define: {
